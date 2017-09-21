134413067a9594e1c594ece669dda773716d39f0
J Tokuhisa <TokuhisaWinston@Outlook.com>
2017-09-21 17:24:52 -0400

    refactors models and adds new methods for UD

--- pantry/models/recipe.js
+++ pantry/models/recipe.js
@@ -6,28 +6,53 @@ const Recipe = {};
 
 // Lists all entries
 Recipe.findAll = () => {
-  return db.query(
-    `SELECT * FROM recipes`
-  );
+  return db.many(`
+    SELECT *
+      FROM recipes
+  ORDER BY id
+  `);
 };
 
 // Finds a specific entry
-Recipe.findById = (id) => {
-  return db.oneOrNone(`
-    SELECT * FROM recipes
-    WHERE id = $1`,
-    [id]
-  );
+Recipe.findById = id => {
+  return db.one(`
+    SELECT *
+      FROM recipes
+     WHERE id = $1
+  `, id);
 };
 
 // Makes a new entry
-Recipe.create = (recipe) => {
+Recipe.save = recipe => {
   return db.one(`
-    INSERT INTO recipes (name, url, img)
-    VALUES ($1, $2, $3)
-    RETURNING *`,
-    [recipe.name, recipe.url, recipe.img]
-  );
+    INSERT INTO recipes
+    (name, url, img)
+    VALUES
+    ($/name/, $/url/, $/img/)
+    RETURNING *
+  `, recipe);
+};
+
+// Edits an existing entry
+// Recipe.update = recipe => {
+//   return db.one(`
+//     UPDATE recipes
+//     SET
+//     name = $/name/,
+//     url = $/url/,
+//     img = $/img/
+//     WHERE id = $/id/
+//     RETURNING *
+//   `, recipe);
+// };
+
+// Removes an existing entry
+Recipe.destroy = id => {
+  return db.none(`
+    DELETE
+      FROM recipes
+     WHERE id = $1
+  `, id);
 };
 
 // Shares functionality with the rest of the app
