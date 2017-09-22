239bd122110711a05fd0b00a523f9ad65bc9c021
J Tokuhisa <TokuhisaWinston@Outlook.com>
2017-09-21 21:43:54 -0400

    creates auth-routes.js inside of routes directory

---
+++ pantry/routes/auth-routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const authRouter = express.Router();
+const passport = require('../services/auth/auth-helpers');
+const usersController = require('../controllers/users-controller');
+
+authRouter.get('/login', authHelpers.loginRedirect, (req, res) => {
+  res.render('auth/login');
+});
+
+authRouter.get('/register', authHelpers.loginRedirect, (req, res) => {
+  res.render('auth/register');
+});
